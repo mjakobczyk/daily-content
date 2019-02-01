da6a36835330e0a88d7a682082b4a82e7688dd41
MJ <Majki@MacBook-Pro-Beata.local>
2019-02-01 20:14:57 +0100

    Add server and config

---
+++ web/config.go
@@ -0,0 +1,10 @@
+package web
+
+type Config struct {
+	Port int    `envconfig:"default=8080"`
+	IP   string `envconfig:"default=0.0.0.0"`
+
+	Logger struct {
+		Type logger.Type `envconfig:"default=3"` // TODO: consider changing logger to include request date
+	}
+}
---
+++ web/server.go
@@ -0,0 +1,9 @@
+package web
+
+// Server struct holds connectors and settings
+type Server struct {
+}
+
+func NewServer(config *Config) *Server {
+
+}
