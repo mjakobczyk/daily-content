ec561dd94af9aa3727919664bca439412cc59bfb
Michał Jakóbczyk <m.jakob@wp.pl>
2019-03-06 09:01:19 +0100

    Web (#3)
    * Add server and config
    
    * Configure packages
    
    * Add examplary handler to the server

--- main.go
+++ main.go
@@ -4,18 +4,22 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/daily-content/web"
+
 	"github.com/vrischmann/envconfig"
 )
 
 func main() {
 	var config Config
+	var err error
 
-	err := envconfig.Init(&config)
+	err = envconfig.Init(&config)
+	fmt.Println("Config: ", config)
 
+	srv := web.NewServer(&config.Server)
+	err = srv.Start()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	fmt.Println("Config:\n", config)
 }
---
+++ web/config.go
@@ -0,0 +1,12 @@
+package web
+
+import "github.com/go-http-utils/logger"
+
+type Config struct {
+	Port int    `envconfig:"default=8080"`
+	IP   string `envconfig:"default=0.0.0.0"`
+
+	Logger struct {
+		Type logger.Type `envconfig:"default=3"` // TODO: consider changing logger to include request date
+	}
+}
